de9c8587e7bc288374bd518171de20275ad734a4
Quan Tran <anhquan0412@gmail.com>
2017-11-26 23:09:57 -0500

    fix for keras2 env. Add notes to neural-style notebook

--- deeplearning2/vgg16_avg.py
+++ deeplearning2/vgg16_avg.py
@@ -35,7 +35,7 @@ def VGG16_Avg(include_top=True, weights='imagenet',
                                       default_size=224,
                                       min_size=48,
                                       data_format=K.image_data_format(),  # Keras2 dim_ordering=K.image_dim_ordering(),
-                                      include_top=include_top)
+                                      require_flatten=include_top) 
 
     if input_tensor is None:
         img_input = Input(shape=input_shape)
